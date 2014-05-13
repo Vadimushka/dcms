4212df67d460a744ed943dcd4db0ab5b31827057
Дмитрий <desure@mail.ru>
2014-05-13 16:46:37 +0600

    Тема оформления "Light Touch": Добавлено левое меню.

--- sys/themes/light_touch/tpl/document.tpl.php
+++ sys/themes/light_touch/tpl/document.tpl.php
@@ -1,102 +1,52 @@
-<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
-<html data-ng-app="" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?= $lang->xml_lang ?>">
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?= $lang->xml_lang ?>">
 <head>
     <title><?= $title ?></title>
     <link rel="shortcut icon" href="/favicon.ico"/>
     <link rel="stylesheet" href="/sys/themes/.common/system.css" type="text/css"/>
     <link rel="stylesheet" href="/sys/themes/.common/theme_light.css" type="text/css"/>
-    <script src="/sys/themes/.common/angular.min.js" async="async"></script>
     <link rel="stylesheet" href="<?= $path ?>/style.css" type="text/css"/>
     <meta http-equiv="content-Type" content="application/xhtml+xml; charset=utf-8"/>
     <meta name="viewport" content="minimum-scale=1.0,initial-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
     <meta name="generator" content="DCMS <?= $dcms->version ?>"/>
-    <script>
-        function vibrate(ms) {
-            if (window.navigator.vibrate)
-                window.navigator.vibrate(ms);
-        }
-        function onNewMail() {
-            vibrate([100, 100]);
-            var audio = document.querySelector("#audio_notify");
-            audio.pause();
-            audio.loop = false;
-            audio.currentTime = 0;
-            audio.play();
-        }
-        function onNewFriend() {
-            vibrate(200);
-        }
-
-        function DcmsCtrl($scope, $interval, $http) {
-            var dcms = $scope.dcms = {
-                interval: 7000,
-                menu: false,
-                user: {
-                    friend_new_count: "<?=$user->friend_new_count?>",
-                    mail_new_count: "<?=$user->mail_new_count?>",
-                    login: "<?=$user->login?>",
-                    id: "<?=$user->id?>",
-                    group: "<?=$user->group?>"
-                },
-                onContainerClick: function ($event) {
-                    if (dcms.menu) {
-                        dcms.menu = false;
-                    }
-                },
-                getFriends: function () {
-                    return dcms.user.friend_new_count != "0" ? "+" + dcms.user.friend_new_count : "";
-                },
-                getMails: function () {
-                    return dcms.user.mail_new_count != "0" ? "+" + dcms.user.mail_new_count : "";
-                },
-                getMailHref: function(){
-                    return '/my.mail.php' + (dcms.user.mail_new_count != "0" ? "?only_unreaded" : "");
-                }
-            }
-            if (dcms.user.group) {
-                $interval(function () {
-                    $http.get('/ajax/user.json.php?' + Object.keys(dcms.user).join("&amp;"))
-                        .success(function ($data) {
-                            if (!$data.group || $data.id != dcms.user.id) {
-                                // слетела авторизация или изменился пользователь. рефрешим страницу
-                                document.location.href = document.location.href;
-                                return;
-                            }
-
-                            if ($data.mail_new_count > dcms.user.mail_new_count) {
-                                onNewMail();
-                            }
-                            if ($data.friend_new_count > dcms.user.friend_new_count) {
-                                onNewFriend();
-                            }
-
-                            dcms.user = $data;
-                            dcms.interval = 7000;
-                        })
-                        .error(function () {
-                            dcms.interval = 60000; // увеличиваем интервал запросов при ошибке
-                        });
-                }, dcms.interval);
-            }
-        }
-    </script>
     <? if ($description) { ?>
         <meta name="description" content="<?= $description ?>" /><? } ?>
     <? if ($keywords) { ?>
         <meta name="keywords" content="<?= $keywords ?>" /><? } ?>
-    <style>
-        .hide {
-            display: none !important;
-        }
-    </style>
+    <script>
+        var translate = {
+            'friends': "<?=__("Друзья")?>",
+            'mail': "<?=__("Почта")?>",
+            'user_menu': "<?= __("Личное меню") ?>",
+            'auth': "<?= __("Авторизация") ?>",
+            'reg': "<?= __("Регистрация") ?>"
+        };
+
+        var user_ajax_url = "/ajax/user.json.php";
+
+        var user = {
+            'id': "<?=$user->id?>",
+            'group': "<?=$user->group?>",
+            'friend_new_count': "<?=$user->friend_new_count?>",
+            'mail_new_count': "<?=$user->mail_new_count?>",
+            'login': "<?=$user->login?>"
+        };
+
+        var URL = "<?=URL?>";
+    </script>
+    <script src="/sys/themes/.common/jquery-2.1.1.js"></script>
+    <script src="<?= $path ?>/js.js"></script>
 </head>
+<body class="theme_light">
 <audio id="audio_notify" preload="auto">
     <source src="/sys/themes/.common/notify.mp3"/>
 </audio>
-<body class="theme_light" data-ng-controller="DcmsCtrl">
-<div id="container_content" data-ng-class="{menu:dcms.menu}">
-    <? $this->display('inc.title.tpl') ?>
-    <? # $this->display('inc.user.tpl') ?>
+<div id="container_content">
+    <h1 id='title' class="gradient_blue">
+        <span id="icon_menu"></span>
+        <span><?= $title ?></span>
+    </h1>
+
     <div id="content">
         <? $this->display('inc.adt.top.tpl') ?>
         <div id="messages">
@@ -112,9 +62,14 @@
         <?= $copyright ?>
     </div>
 </div>
-<div id="container_overflow" data-ng-class="{menu:dcms.menu}" data-ng-click="dcms.onContainerClick($event)"></div>
-<div id="container_menu" data-ng-class="{menu:dcms.menu}">
-    <? $this->display('inc.menu.tpl') ?>
+<div id="container_overflow"></div>
+<div id="container_menu">
+    <span id="user" class="gradient_blue"><?= $user->login ?></span>
+    <a id="menu_user" class="gradient_grey" href="/menu.user.php"></a>
+    <a id="my_friends" class="gradient_grey" href='/my.friends.php'></a>
+    <a id="my_mail" class="gradient_grey" href='/my.mail.php?only_unreaded'></a>
+    <a id="login" class="gradient_grey" href="/login.php?return=<?= URL ?>"></a>
+    <a id="reg" class="gradient_grey" href="/reg.php?return=<?= URL ?>"></a>
 </div>
 </body>
 </html>
