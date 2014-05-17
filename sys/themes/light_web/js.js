abecbfd008fcf73f6482b796c42e678ace7f0862
DESURE <desure@mail.ru>
2014-05-17 13:54:21 +0600

    Light Web XmlHttpRequest header, чтобы счетчик переходов не увеличивался при каждом запросе.

--- sys/themes/light_web/js.js
+++ sys/themes/light_web/js.js
@@ -58,7 +58,8 @@ angular.module('Dcms', ['monospaced.elastic', 'ngAnimate'])
         };
     })
     .config(function ($httpProvider) {    // [url]http://habrahabr.ru/post/181009/[/url]
-        $httpProvider.defaults.headers.post[ 'Content-Type' ] = 'application/x-www-form-urlencoded;charset=utf-8';
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
         $httpProvider.defaults.transformRequest = function (data) {
             return angular.isObject(data) && String(data) !== '[object File]' ? $.param(data) : data;
         };
