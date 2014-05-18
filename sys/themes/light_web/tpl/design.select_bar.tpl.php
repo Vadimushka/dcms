56eab383a86fa2cc8348e04fa0c41063b7486ddf
Дмитрий <desure@mail.ru>
2014-05-18 18:02:01 +0600

    Issue #30

--- sys/themes/light_web/tpl/design.select_bar.tpl.php
+++ sys/themes/light_web/tpl/design.select_bar.tpl.php
@@ -4,7 +4,7 @@
         if (empty($option[2]))
             echo '<a class="gradient_grey border radius padding" href="'.$option[0].'">'.$option[1].'</a>';
         else
-            echo '<span class="gradient_grey border radius padding">'.$option[1].'</span>';
+            echo '<span class="gradient_grey border radius padding invert">'.$option[1].'</span>';
     }
     ?>
 </div>
