72836d52d6be44c14e8173658075ef251c18008c
Дмитрий <desure@mail.ru>
2014-07-12 18:23:44 +0600

    Статистика для мобильной темы

---
+++ sys/themes/default_mobile/tpl/chart.line.tpl.php
@@ -0,0 +1,39 @@
+<div id="<?= $id ?>" class="chart"></div>
+<script>
+    $(document).on('highchartsLoaded', function () {
+        $('#<?=$id?>').highcharts({
+            title: {
+                text: <?=json_encode($title)?>
+            },
+            xAxis: {
+                categories: <?=json_encode($categories)?>
+            },
+            yAxis: {
+                title: {
+                    text: <?=json_encode($y_text)?>
+                },
+                plotLines: [
+                    {
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }
+                ]
+            },
+            tooltip: {
+                valueSuffix: <?=json_encode($value_suffix)?>
+            },
+            series: <?=json_encode($series)?>
+        });
+    });
+
+    $(function () {
+        if ($.fn.highcharts || window.highchartsLoading)
+            return;
+        window.highchartsLoading = true;
+        $.getScript('/sys/themes/.common/highcharts.js', function () {
+            delete window.highchartsLoading;
+            $(document).trigger('highchartsLoaded');
+        });
+    });
+</script>
