f3ef823e06caeb9324b96f614068ac374c83cc6c
DESURE <desure@mail.ru>
2014-12-05 17:26:29 +0600

    Удаление xmlns для шаблонов почтовых писем.

--- sys/templates/mail.news.tpl.php
+++ sys/templates/mail.news.tpl.php
@@ -1,23 +1,23 @@
-<html lang="ru" xml:lang="ru" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title><?= $title ?> - <?= $site ?></title>
-    <style type="text/css">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title><?= $title ?> - <?= $site ?></title>
+        <style type="text/css">
             /* <![CDATA[ */
-        body {
-            font-family: tahoma, arial, verdana, sans-serif, Lucida Sans;
-            font-size: 14px;
-        }
-        #unsubscribe{
-            font-size: small;
-        }
+            body {
+                font-family: tahoma, arial, verdana, sans-serif, Lucida Sans;
+                font-size: 14px;
+            }
+            #unsubscribe{
+                font-size: small;
+            }
 
             /* ]]> */
-    </style>
-</head>
-<body>
-<div>Уведомляем Вас о новостях:</div>
-<p><?= $content ?></p>
-<div id="unsubscribe">Если вы больше не хотите получать сообщения о новостях на email <?= $email ?>, перейдите по <a href="<?= $unsubscribe ?>">ссылке</a>.</div>
-</body>
+        </style>
+    </head>
+    <body>
+        <div>Уведомляем Вас о новостях:</div>
+        <p><?= $content ?></p>
+        <div id="unsubscribe">Если вы больше не хотите получать сообщения о новостях на email <?= $email ?>, перейдите по <a href="<?= $unsubscribe ?>">ссылке</a>.</div>
+    </body>
 </html>
