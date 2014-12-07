d0b54e4325933212c6fa10ea27ba4637a0521689
DESURE <desure@mail.ru>
2014-12-08 03:10:17 +0500

    fix мобильной темы

--- sys/themes/default_mobile/tpl/document.tpl.php
+++ sys/themes/default_mobile/tpl/document.tpl.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="/sys/themes/.common/system.css" type="text/css"/>
     <link rel="stylesheet" href="/sys/themes/.common/icons.css" type="text/css"/>
     <link rel="stylesheet" href="/sys/themes/.common/theme_light.css" type="text/css"/>
-    <link rel="stylesheet" href="<?= $path ?>/style.css?3" type="text/css"/>
+    <link rel="stylesheet" href="<?= $path ?>/style.css?5" type="text/css"/>
     <meta http-equiv="content-Type" content="application/xhtml+xml; charset=utf-8"/>
     <meta name="viewport" content="minimum-scale=1.0,initial-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
     <meta name="generator" content="DCMS <?= $dcms->version ?>"/>
@@ -40,7 +40,7 @@
     </script>
     <script src="/sys/themes/.common/jquery-2.1.1.min.js"></script>
     <script src="/sys/themes/.common/dcmsApi.js"></script>
-    <script src="<?= $path ?>/js.js?4"></script>
+    <script src="<?= $path ?>/js.js?5"></script>
 </head>
 <body class="theme_light">
 <audio id="audio_notify" preload="auto">
@@ -51,11 +51,11 @@
         <span id="icon_menu"></span>
         <span><?= $title ?></span>
     </h1>
-    <? $this->displaySection('after_title')?>
+    <? $this->displaySection('after_title') ?>
     <div id="tabs">
         <?= $this->section($tabs, '<a class="gradient_grey border tab sel{selected}" href="{url}">{name}</a>', true); ?>
     </div>
-    <? $this->displaySection('before_content')?>
+    <? $this->displaySection('before_content') ?>
     <div id="content">
         <div id="messages">
             <?= $this->section($err, '<div class="gradient_red border radius">{text}</div>'); ?>
@@ -63,7 +63,7 @@
         </div>
         <?php $this->displaySection('content') ?>
     </div>
-    <? $this->displaySection('after_content')?>
+    <? $this->displaySection('after_content') ?>
     <? $this->display('inc.foot.tpl') ?>
     <div id="foot">
         <?= __("Время генерации страницы: %s сек", $document_generation_time) ?><br/>
@@ -73,7 +73,9 @@
 <div id="container_overflow"></div>
 <div id="container_menu">
     <span id="user" class="gradient_blue"><?= $user->nick ?></span>
-    <? $this->displaySection('menu')?>
+    <a id="login" class="gradient_grey" href="/login.php?return=<?= URL ?>"></a>
+    <a id="reg" class="gradient_grey" href="/reg.php?return=<?= URL ?>"></a>
+    <? $this->displaySection('menu') ?>
 </div>
 </body>
 </html>
