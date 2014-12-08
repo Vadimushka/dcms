38c605f2c96351ed26c16d7a11962c6b92830d60
DESURE <desure@mail.ru>
2014-12-08 14:07:49 +0500

    Заголовок писался в $doc->theme O_o

--- forum/message.edit.php
+++ forum/message.edit.php
@@ -2,47 +2,50 @@
 
 include_once '../sys/inc/start.php';
 $doc = new document(1);
-$doc->theme = __('Редактирование сообщения');
+$doc->title = __('Редактирование сообщения');
 
 if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
-    if (isset($_GET['return']))
+    if (isset($_GET['return'])) {
         header('Refresh: 1; url=' . $_GET['return']);
-    else
+    } else {
         header('Refresh: 1; url=./');
+    }
     $doc->err(__('Ошибка выбора сообщения'));
     exit;
 }
 
-$id_message = (int) $_GET['id'];
+$id_message = (int)$_GET['id'];
 $q = $db->prepare("SELECT * FROM `forum_messages` WHERE `id` = ?");
 $q->execute(Array($id_message));
 
 if (!$message = $q->fetch()) {
-    if (isset($_GET['return']))
+    if (isset($_GET['return'])) {
         header('Refresh: 1; url=' . $_GET['return']);
-    else
+    } else {
         header('Refresh: 1; url=./');
+    }
     $doc->err(__('Сообщение не найдено'));
 
     exit;
 }
-$autor = new user((int) $message['id_user']);
+$autor = new user((int)$message['id_user']);
 
 $access_edit = false;
 $edit_time = $message['time'] - TIME + 600;
 
-if ($user->group > $autor->group || $user->group == groups::max())
+if ($user->group > $autor->group || $user->group == groups::max()) {
     $access_edit = true;
-elseif ($user->id == $autor->id && $edit_time > 0) {
+} elseif ($user->id == $autor->id && $edit_time > 0) {
     $access_edit = true;
     $doc->msg(__('Для изменения сообщения осталось %d сек', $edit_time));
 }
 
 if (!$access_edit) {
-    if (isset($_GET['return']))
+    if (isset($_GET['return'])) {
         header('Refresh: 1; url=' . $_GET['return']);
-    else
+    } else {
         header('Refresh: 1; url=./');
+    }
     $doc->err(__('Сообщение не доступно для редактирования'));
     exit;
 }
@@ -51,32 +54,36 @@ if (!$access_edit) {
 $doc->title = __('Сообщение от "%s" - редактирование', $autor->login);
 
 if (isset($_GET['act']) && $_GET['act'] == 'hide') {
-    if (isset($_GET['return']))
+    if (isset($_GET['return'])) {
         header('Refresh: 1; url=' . $_GET['return']);
-    else
+    } else {
         header('Refresh: 1; url=theme.php?id=' . $message['id_theme']);
+    }
     $res = $db->prepare("UPDATE `forum_messages` SET `group_show` = '2' WHERE `id` = ? LIMIT 1");
     $res->execute(Array($message['id']));
     $doc->msg(__('Сообщение успешно скрыто'));
-    if (isset($_GET['return']))
+    if (isset($_GET['return'])) {
         $doc->ret(__('В тему'), text::toValue($_GET['return']));
-    else
+    } else {
         $doc->ret(__('В тему'), 'theme.php?id=' . $message['id_theme']);
+    }
     exit;
 }
 
 if (isset($_GET['act']) && $_GET['act'] == 'show') {
-    if (isset($_GET['return']))
+    if (isset($_GET['return'])) {
         header('Refresh: 1; url=' . $_GET['return']);
-    else
+    } else {
         header('Refresh: 1; url=theme.php?id=' . $message['id_theme']);
+    }
     $res = $db->prepare("UPDATE `forum_messages` SET `group_show` = '0' WHERE `id` = ? LIMIT 1");
     $res->execute(Array($message['id']));
     $doc->msg(__('Сообщение будет отображаться'));
-    if (isset($_GET['return']))
+    if (isset($_GET['return'])) {
         $doc->ret('В тему', text::toValue($_GET['return']));
-    else
+    } else {
         $doc->ret(__('В тему'), 'theme.php?id=' . $message['id_theme']);
+    }
     exit;
 }
 
@@ -88,21 +95,28 @@ if (isset($_POST['message'])) {
     } elseif ($dcms->censure && $mat = is_valid::mat($message_new)) {
         $doc->err(__('Обнаружен мат: %', $mat));
     } elseif ($message_new) {
-        if (isset($_GET['return']))
+        if (isset($_GET['return'])) {
             header('Refresh: 1; url=' . $_GET['return']);
-        else
+        } else {
             header('Refresh: 1; url=theme.php?id=' . $message['id_theme']);
+        }
 
         $res = $db->prepare("INSERT INTO `forum_history` (`id_message`, `id_user`, `time`, `message`) VALUES (?,?,?,?)");
-        $res->execute(Array($message['id'], ($message['edit_id_user'] ? $message['edit_id_user'] : $message['id_user']), ($message['edit_time'] ? $message['edit_time'] : $message['time']), $message['message']));
+        $res->execute(Array(
+                $message['id'],
+                ($message['edit_id_user'] ? $message['edit_id_user'] : $message['id_user']),
+                ($message['edit_time'] ? $message['edit_time'] : $message['time']),
+                $message['message']
+            ));
         $res = $db->prepare("UPDATE `forum_messages` SET `message` = ?, `edit_count` = `edit_count` + 1, `edit_id_user` = ?, `edit_time` = ? WHERE `id` = ? LIMIT 1");
         $res->execute(Array($message_new, $user->id, TIME, $message['id']));
         $doc->msg(__('Сообщение успешно изменено'));
 
-        if (isset($_GET['return']))
+        if (isset($_GET['return'])) {
             $doc->ret('В тему', text::toValue($_GET['return']));
-        else
+        } else {
             $doc->ret(__('В тему'), 'theme.php?id=' . $message['id_theme']);
+        }
         exit;
     } else {
         $doc->err(__('Нельзя оставить пустое сообщение'));
@@ -114,10 +128,11 @@ $form->textarea('message', __('Редактирование сообщения')
 $form->button(__('Применить'));
 $form->display();
 
-$doc->act(__('Вложения'), 'message.files.php?id=' . $message['id'] . (isset($_GET['return']) ? '&amp;return=' . urlencode($_GET['return']) : null));
+$doc->act(__('Вложения'),
+    'message.files.php?id=' . $message['id'] . (isset($_GET['return']) ? '&amp;return=' . urlencode($_GET['return']) : null));
 
-if (isset($_GET['return']))
+if (isset($_GET['return'])) {
     $doc->ret(__('В тему'), text::toValue($_GET['return']));
-else
+} else {
     $doc->ret(__('В тему'), 'theme.php?id=' . $message['id_theme']);
-?>
+}
