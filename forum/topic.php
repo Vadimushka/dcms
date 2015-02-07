67f313f9a871568392c2bd3350d26f6f8844c122
DESURE <desure@mail.ru>
2015-02-07 22:47:38 +0500

    Оптимизация запроса на вывод спискак тем в разделах форума

--- forum/topic.php
+++ forum/topic.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once '../sys/inc/start.php';
+include 'inc/functions.php';
 $doc = new document();
 
 $doc->title = __('Форум');
@@ -13,13 +14,12 @@ if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
 $id_top = (int)$_GET['id'];
 
 $q = $db->prepare("SELECT `forum_topics`.*, `forum_categories`.`name` AS `category_name` FROM `forum_topics`
-LEFT JOIN `forum_categories` ON `forum_categories`.`id` = `forum_topics`.`id_category`
+JOIN `forum_categories` ON `forum_categories`.`id` = `forum_topics`.`id_category`
 WHERE `forum_topics`.`id` = ? AND `forum_topics`.`group_show` <= ? AND `forum_categories`.`group_show` <= ?");
 $q->execute(Array($id_top, $user->group, $user->group));
 if (!$topic = $q->fetch()) {
     header('Refresh: 1; url=./');
     $doc->err(__('Раздел не доступен'));
-
     exit;
 }
 
@@ -31,12 +31,9 @@ $posts = array();
 $pages = new pages;
 $pages->posts = $res->fetchColumn();
 
-$q = $db->prepare("SELECT `forum_themes`.* ,
-        COUNT(`forum_messages`.`id`) AS `count`,
-        (SELECT COUNT(`fv`.`id_user`) FROM `forum_views` AS `fv` WHERE `fv`.`id_theme` = `forum_themes`.`id`)  AS `views`
+$q = $db->prepare("SELECT `forum_themes`.*
         FROM `forum_themes`
-LEFT JOIN `forum_messages` ON `forum_messages`.`id_theme` = `forum_themes`.`id`
-
+JOIN `forum_messages` ON `forum_messages`.`id_theme` = `forum_themes`.`id`
  WHERE `forum_themes`.`id_topic` = ? AND `forum_themes`.`group_show` <= ? AND `forum_messages`.`group_show` <= ?
  GROUP BY `forum_themes`.`id`
  ORDER BY `forum_themes`.`top`, `forum_themes`.`time_last` DESC LIMIT " . $pages->limit);
@@ -44,23 +41,31 @@ $q->execute(Array($topic['id'], $user->group, $user->group));
 $listing = new listing();
 
 if ($arr = $q->fetchAll()) {
-    foreach ($arr AS $themes) {
+
+    $themes_ids = array();
+    foreach ($arr AS $theme) {
+        $themes_ids[] = $theme['id'];
+    }
+    $themes_msg_counters = forum_getMessagesCounters($themes_ids, 0, current_user::getInstance()->group);
+    $themes_views_counters = forum_getViewsCounters($themes_ids);
+
+    foreach ($arr AS $theme) {
         $post = $listing->post();
 
-        $is_open = (int)($themes['group_write'] <= $topic['group_write']);
+        $is_open = (int)($theme['group_write'] <= $topic['group_write']);
 
-        $post->icon("forum.theme.{$themes['top']}.$is_open");
-        $post->title = text::toValue($themes['name']);
-        $post->url = 'theme.php?id=' . $themes['id'];
-        $post->counter = $themes['count'];
-        $post->time = misc::when($themes['time_last']);
+        $post->icon("forum.theme.{$theme['top']}.$is_open");
+        $post->title = text::toValue($theme['name']);
+        $post->url = 'theme.php?id=' . $theme['id'];
+        $post->counter = $themes_msg_counters[$theme['id']];
+        $post->time = misc::when($theme['time_last']);
 
 
-        $autor = new user($themes['id_autor']);
-        $last_msg = new user($themes['id_last']);
+        $autor = new user($theme['id_autor']);
+        $last_msg = new user($theme['id_last']);
 
         $post->content = ($autor->id != $last_msg->id ? $autor->nick . '/' . $last_msg->nick : $autor->nick) . '<br />';
-        $post->bottom = __('Просмотров: %s', $themes['views']);
+        $post->bottom = __('Просмотров: %s', $themes_views_counters[$theme['id']]);
     }
 }
 
