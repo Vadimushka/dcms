d479e6d8460839e3e7f32b2a5b85acedb1c07263
S1S13AF7 <>
2015-07-14 14:39:08 +0300

    Именинники, сортировка по возрасту

--- pages/users.php
+++ pages/users.php
@@ -3,18 +3,30 @@
 include_once '../sys/inc/start.php';
 $doc = new document();
 $doc->title = __('Пользователи');
-
-switch (@$_GET['order']) {
+$none = __('Нет пользователей');
+switch ($order = (string) @$_GET['order']) {
+    case 'birthdays':
+        $sort = 'DESC';
+        $order = 'ank_g_r';
+        $where = "WHERE `ank_m_r` = '".date("m")."' AND `ank_d_r` = '".date("d")."'";
+        $none = __('Сегодня нет именинников');
+        $doc->title = __('Именинники');
+        break;
+    case 'age':
+        $order = "ank_g_r";
+        $sort = 'DESC,`ank_m_r` DESC, `ank_d_r` DESC';
+        $none = __('Нет пользователей, указавших дату рождения');
+        $where = "WHERE `ank_g_r` > '0' AND `ank_m_r` > '0' AND `ank_d_r` > '0'";
+        break;
     case 'donate_rub':
-        $order = 'donate_rub';
         $sort = 'DESC';
         $where = "WHERE `donate_rub` > '0'";
+        $none = __('Нет пользователей, пожертвовавших денег на проект');
         $doc->title = __('Пожертвования');
         break;
     case 'group':
-        $order = 'group';
         $sort = 'DESC';
-        $where = "WHERE `group` >= '2'";
+        $where = "WHERE `group` > '1'";
         $doc->title = __('Администрация');
         break;
     case 'login':
@@ -53,14 +65,22 @@ $pages = new pages;
 
 $res = $db->query("SELECT COUNT(*) FROM `users` $where");
 $pages->posts = $res->fetchColumn();
+
+if ((string) @$_GET['order'] != 'birthdays') {
+   /* проверяем есть-ли сегодня именинники */
+   $res = $db->query("SELECT COUNT(*) FROM `users` WHERE `ank_m_r` = '".date("m")."' AND `ank_d_r` = '".date("d")."'");
+   $bds = $res->fetchColumn(); } else {
+   $bds = $pages->posts; }
+
 // меню сортировки
 $ord = array();
-$ord[] = array("?order=id&amp;page={$pages->this_page}" . (isset($search) ? '&amp;search=' . urlencode($search) : ''), __('ID пользователя'), $order == 'id');
-$ord[] = array("?order=login&amp;page={$pages->this_page}" . (isset($search) ? '&amp;search=' . urlencode($search) : ''), __('Логин'), $order == 'login');
+$ord[] = array("?order=id&amp;page={$pages->this_page}" . (isset($search) ? '&amp;search=' . urlencode($search) : ''), __('ID пользователя'), $order == 'id'); if ($bds || (string) @$_GET['order'] == 'birthdays')
+$ord[] = array("?order=birthdays&amp;page={$pages->this_page}" . (isset($search) ? '&amp;search=' . urlencode($search) : ''), __('Именинники'), (string) @$_GET['order'] == 'birthdays');
 $ord[] = array("?order=rating&amp;page={$pages->this_page}" . (isset($search) ? '&amp;search=' . urlencode($search) : ''), __('Рейтинг'), $order == 'rating');
 $ord[] = array("?order=balls&amp;page={$pages->this_page}" . (isset($search) ? '&amp;search=' . urlencode($search) : ''), __('Баллы'), $order == 'balls');
 $ord[] = array("?order=group&amp;page={$pages->this_page}" . (isset($search) ? '&amp;search=' . urlencode($search) : ''), __('Статус'), $order == 'group');
-$ord[] = array("?order=donate_rub&amp;page={$pages->this_page}" . (isset($search) ? '&amp;search=' . urlencode($search) : ''), __('Пожертвования'), $order == 'donate_rub');
+$ord[] = array("?order=login&amp;page={$pages->this_page}" . (isset($search) ? '&amp;search=' . urlencode($search) : ''), __('Логин'), $order == 'login');
+$ord[] = array("?order=age&amp;page={$pages->this_page}" . (isset($search) ? '&amp;search=' . urlencode($search) : ''), __('Возраст'), (string) @$_GET['order'] == 'age');
 $or = new design();
 $or->assign('order', $ord);
 $or->display('design.order.tpl');
@@ -90,13 +110,20 @@ if ($arr = $q->fetchAll()) {
             case 'balls':
                 $post->content[] = '[b]' . __('Баллы') . ': ' . ((int)$p_user->balls) . '[/b]';
                 break;
+            case 'ank_g_r':
+                $post->content[] = '[b]' . (((int) $p_user->ank_g_r > 0) ? __('Дата рождения') . ": $p_user->ank_d_r.$p_user->ank_m_r.$p_user->ank_g_r (" . misc::get_age($p_user->ank_g_r, $p_user->ank_m_r, $p_user->ank_d_r) . ')' : __('День рождения').": $p_user->ank_d_r " . misc::getLocaleMonth($p_user->ank_m_r)) . '[/b]';
+                break;
             case 'donate_rub':
                 $post->content[] = '[b]' . __('Сумма пожертвований: %s руб.', $p_user->donate_rub) . '[/b]';
                 break;
         }
 
         $post->content[] = '[small]' . __('Дата регистрации') . ': ' . date('d-m-Y', $p_user->reg_date) . '[/small]';
-        $post->content[] = '[small]' . __('Последний визит') . ': ' . misc::when($p_user->last_visit) . '[/small]';
+        $post->content[] = '[small]' . __('Последний визит') . ': ' . ($p_user->last_visit ? misc::when($p_user->last_visit) : misc::when($p_user->reg_date)) . '[/small]';
+    }
+
+    if ($order == 'ank_g_r') { /* fix */
+        $order = (string) $_GET['order'];
     }
 }
 
@@ -106,6 +133,6 @@ $form->text('search', __('Ник или его часть'), @$search, false);
 $form->button(__('Поиск'));
 $form->display();
 
-$listing->display($order == 'donate_rub' ? __('Нет пользователей, пожертвовавших денег на проект') : __('Нет пользователей'));
+$listing->display($none);
 
 $pages->display("?order=$order&amp;" . (isset($search) ? 'search=' . urlencode($search) . '&amp;' : ''));
