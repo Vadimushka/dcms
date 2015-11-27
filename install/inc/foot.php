a5e8e32bad7c14fdca3aec2a90e9888fe4ed5769
S1S13AF7 <>
2015-11-27 11:36:13 +0200

    ссылка в "ногах" инсталятора (копирайт) откроет оф.сайт в новой вкладке, на языке, который выбран для инсталяции, по тому-же протоколу, что и производится установка (большенство современных браузеров это поддерживают, а несовременные всеравно движком не поддерживаются)

--- install/inc/foot.php
+++ install/inc/foot.php
@@ -1,5 +1,5 @@
-<div class="page_foot">
-<a href="http://dcms.su">DCMS.SU</a>
+﻿<div class="page_foot">
+© <a href="//DCMS.su/language.php?return=%2F&set_lang=<?php echo $user_language_pack->code;?>" target="_blank">DCMS</a> Group
 </div>
 </body>
 </html>
