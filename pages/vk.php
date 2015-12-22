cb5fc82a0695b6a8a47f062f44dd9a31d0fc4071
S1S13AF7 <>
2015-12-22 03:58:59 +0200

    Долго думал куда-же впихнуть ф-ю.. как насчет misc::logaut($id, $method, $status, $vk_id = 0)? Пока что сбор идет только с vk.php и вродь как успешно.

--- pages/vk.php
+++ pages/vk.php
@@ -27,26 +27,6 @@ if (!$dcms->vk_app_id || !$dcms->vk_app_secret){
     exit;
 }
 
-if(!function_exists('logaut')) {
-function logaut($id, $method, $status, $vk_id) {
-        global $db, $dcms; /* будем получать IP, ID браузера, домен, делать запросы */
-        $ua = (string) @$_SERVER['HTTP_USER_AGENT']; // (string) USER_AGENT
-        
-        $q = $db->prepare("SELECT * FROM `log_of_user_aut` WHERE `id_user` = :id AND `user_id' = :uid AND `iplong` = :ip_long AND `browser_ua` = :ua AND `domain` = :domain AND `method` = :method AND `status` = :status ORDER BY `time` DESC LIMIT 1");
-        $q->execute(Array(':id' => $id, ':ip_long' => $dcms->ip_long, ':ua' => $ua, ':domain' => $dcms->subdomain_main, ':method' => $method, ':status' => $status));
-
-        if(!$row = $q->fetch()) { /* якщо записи з такими параметрами відсутні, то додаємо у базу */
-//            mysql_query("INSERT INTO `log_of_user_aut` (`id_user`,`method`,`iplong`,`time`,`id_browser`,`browser`,`browser_ua`,`domain`,`status`) VALUES ('$id','$method','$dcms->ip_long','" . TIME . "','$dcms->browser_id','$dcms->browser_name','$ua','" . $dcms->subdomain_main . "','$status')");
-            $res = $db->prepare("INSERT INTO `log_of_user_aut` (`id_user`,`user_id`,`method`,`iplong`, `time`, `id_browser`,`browser`,`browser_ua`,`domain`,`status`) VALUES (:id,:uid,:method,:ip_long,:time,:br_id,:br_name,:ua,:domain,:status)");
-            $res->execute(Array(':id' => $id, ':uid' => $vk_id, ':ip_long' => $dcms->ip_long, ':ua' => $ua, ':domain' => $dcms->subdomain_main, ':method' => $method, ':status' => $status, ':br_id' => $dcms->browser_id, ':br_name' => $dcms->browser_name, ':time' => TIME));
-        } else {
-//            mysql_query("UPDATE `log_of_user_aut` SET `time` = '" . TIME . "', `count` = `count` + 1 WHERE `id_user` = '$id' AND `iplong` = '" . $dcms->ip_long . "' AND `browser_ua` = '$ua' AND `domain` = '" . $dcms->subdomain_main . "' AND `method` = '$method' AND `status` = '$status' LIMIT 1");
-            $res = $db->prepare("UPDATE `log_of_user_aut` SET `time` = :time, `id_browser` = :br_id, `count` = `count` + 1 WHERE `id_user` = :id AND `user_id' = :uid AND `iplong` = :ip_long AND `browser_ua` = :ua AND `domain` = :domain AND `method` = :method AND `status` = :status LIMIT 1");
-            $res->execute(Array(':id' => $id, ':uid' => $vk_id, ':ip_long' => $dcms->ip_long, ':ua' => $ua, ':domain' => $dcms->subdomain_main, ':method' => $method, ':status' => $status, ':br_id' => $dcms->browser_id, ':time' => TIME));
-        }
-    }
-}
-
 try{
     $vk = new vk($dcms->vk_app_id, $dcms->vk_app_secret);
     $vk->getAccessToken('http://' . $_SERVER['HTTP_HOST'] . '/vk.php', $_GET['code']);
@@ -59,7 +39,7 @@ try{
         $q->execute(array(':email' => $vk->getEmail()));
         if ($q->rowCount()) {
             $user_data = $q->fetch();
-            logaut($user_data['id'], 'vk', 1, $vk_user['uid']);
+            misc::logaut($user_data['id'], 'vk', 1, $vk_user['uid']);
             $_SESSION [SESSION_ID_USER] = $user_data['id'];
             $doc->msg(__("Авторизация прошла успешно"));
             exit;
@@ -70,7 +50,7 @@ try{
     $q->execute(array(':id_vk' => $vk_user['uid']));
     if ($q->rowCount()) {
         $user_data = $q->fetch();
-        logaut($user_data['id'], 'vk', 1, $vk_user['uid']);
+        misc::logaut($user_data['id'], 'vk', 1, $vk_user['uid']);
         $_SESSION [SESSION_ID_USER] = $user_data['id'];
         if (empty($user_data['reg_mail']) && $vk->getEmail()) {
                   $q = $db->prepare("UPDATE `users` SET `reg_mail` = :email WHERE `vk_id` = :vk_id LIMIT 1");
@@ -78,7 +58,7 @@ try{
         $doc->msg(__("Авторизация прошла успешно"));
         exit;
     } else if (!$dcms->vk_reg_enable) {
-        logaut(0, 'vk', 0, $vk_user['uid']);
+        misc::logaut(0, 'vk', 0, $vk_user['uid']);
         throw new Exception(__('Регистрация через vk.com запрещена'));
     }
 
